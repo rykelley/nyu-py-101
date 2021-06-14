c2ca290c4749f6086d798bd7a31c2956ef288c15
Ryan Kelley <rykelley@gmail.com>
2021-06-14 10:39:31 -0600

    Looks good

--- Week-1/restraunt-bill.py
+++ Week-1/restraunt-bill.py
@@ -1,27 +1,30 @@
 
+# Prompt user for the charge of the food
 
 
-#Prompt user for the charge of the food
+food_charge = float(input('Enter amount of money for the food: '))       # Input the total food bill and convert to float
 
+num_party = int(input('How many people in your party ?: '))              # This takes the input of
 
-food_charge = float(input('Enter amount of money for the food: '))          #This will convert the number entered into a float (so the user can type in 34.88, etc)
-num_party = int(input('How many people in your party ?: '))
 tip = int(input('Please enter the desired tip percentage (for example,"20" for 20%: '))
 
-tip_total = tip / 100
+tip_total = "%.2f" % float(tip / 100 * food_charge)
 
-total_bill = food_charge * float(tip / 100 +1)
+total_bill = food_charge * float(tip / 100 + 1)
 
 amount_each = (round(float(((tip / 100 + 1) * food_charge) / num_party), 2))
 
-total_bill_float = float(total_bill)
-tip_amount_float = float(tip_total)
-tip_and_total = (total_bill_float+tip_amount_float)
-print(f"Total bill including tip: ${tip_and_total}")
+
 
 print("\n")
 
-print(f"A {tip}% tip (${tip_total}) was added to the bill, for a total of ${tip_and_total}")
+
+
+
+
+print("\n")
+
+print(f"A {tip}% tip (${tip_total}) was added to the bill, for a total of ${total_bill}")
 
 
 
